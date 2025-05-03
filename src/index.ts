66c79ac01034af8a08be2bcac59410642d48b18b
PedroGaya <pedrohenriquegaya@gmail.com>
2025-05-03 09:39:56 -0300

    finish setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
 import express, { Express, Request, Response } from "express";
+import { prisma } from "./prisma";
+
+import { UserData } from "./types";
 
 const app: Express = express();
 const port = 4000;
 
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const users = await prisma.user.findMany();
+
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.post("/users", async (req: Request<{}, {}, UserData>, res: Response) => {
+  try {
+    const { email, name } = req.body;
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+      },
+    });
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send("OK");
 });
 
 app.listen(port, () => {
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type UserData = {
+  email: string;
+  name: string;
+};
